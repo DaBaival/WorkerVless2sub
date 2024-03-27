2e4abec9df68407fed99145587a76f58e7529a48
DaBaival <148878832+DaBaival@users.noreply.github.com>
2024-03-27 16:44:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -16,7 +16,10 @@ let addresses = [
 
 // 设置优选地址api接口
 let addressesapi = [
-	 'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+	
+'https://raw.githubusercontent.com/DaBaival/WorkerVless2sub/main/addressesapi.txt',
+
   'https://addressesapi.090227.xyz/CloudFlareYes',
 //'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
 ];
